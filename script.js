9d4e8ace2cc4ddcb7faef7f0ae6b9d5e08566cb2
Celia Alonso <celiaag94@gmail.com>
2023-03-10 14:46:47 +0000

    button in home page works!

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const elements = [
-    { buttonId: "buttonReadMore", textId: "text2" },
-  ];
+
   
+  const text2 = document.getElementById("text2")
+
+
+const btn = document.getElementById('myButton');
+btn.addEventListener('click', (e) => {
+  text2.classList.toggle('hidden');
+});
